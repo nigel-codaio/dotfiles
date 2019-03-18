d7ebef80746cbda48c647825b166b06d2880560f
Stephen Marquis <marquissm@gmail.com>
2019-03-18 11:36:06 -0700

    Update android path

--- android/path.zsh
+++ android/path.zsh
@@ -1,1 +1,2 @@
-export PATH=$HOME/Library/Android/sdk/tools:$HOME/Library/Android/sdk/platform-tools:$PATH
+export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+export PATH=$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH
