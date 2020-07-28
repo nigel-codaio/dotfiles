0efe40d785e1dafdac2cbce27fae5aa8dfaf54ea
Stephen Marquis <marquissm@gmail.com>
2020-07-28 12:56:54 -0700

    Move to src instead of documents

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,5 @@ alias reload!='. ~/.zshrc'
 
 alias dirs="dirs -v"
 
-function repos(){ repoPath="$HOME/Documents/repos/$1"; cd $repoPath }
+function repos(){ repoPath="$HOME/src/repos/$1"; cd $repoPath }
 function psman(){ rm /tmp/psman.*; man -t $@ > /tmp/psman.ps; pstopdf /tmp/psman.ps; open -a Preview /tmp/psman.pdf }
